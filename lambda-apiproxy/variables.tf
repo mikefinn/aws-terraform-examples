afc4d3d599477ec68a161cd8474253e62499bc1c
mikefinn <mike@finnclan.org>
2019-04-17 10:08:39 -0400

    Added write of URL to file

--- lambda-apiproxy/variables.tf
+++ lambda-apiproxy/variables.tf
@@ -1,3 +1,8 @@
 output "base_url" {
   value = "${aws_api_gateway_deployment.apiproxylambda.invoke_url}"
 }
+
+resource "local_file" "url" {
+    content     = "${aws_api_gateway_deployment.apiproxylambda.invoke_url}"
+    filename = "url.txt"
+}
